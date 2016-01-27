4cf624bf3e4a80a145855afe8aa488906fecdc36
kko13 <kko13@my.fsu.edu>
2016-01-27 11:04:50 -0500

    Add order.txt and setup input
    Added file for testing and setup basic file input.

--- src/HardwoodSellerC.cpp
+++ src/HardwoodSellerC.cpp
@@ -1,32 +1,52 @@
-//============================================================================
+//============================================================================               
 // Name        : HardwoodSellerC.cpp
 // Author      : Esteban Parra
 // Version     : 1
 // Copyright   : It's free
 // Description : Main class for the Hardwood Seller
-//============================================================================
+//============================================================================               
 
 #include <iostream>
+#include <string>
+#include <fstream>
+#include "WoodItem.h"
 using namespace std;
-
-int main() {
-	cout
-			<< "This is an empty main method"
-			<< endl;
-	return 0;
-}
 
 /*
  * Method to read the input file
  */
-void readInputFile(string inputFilePath) {
-
+string* readInputFile(string inputFilePath) {
+        string* lines = new string[2];
+        ifstream file (inputFilePath.c_str());
+        if (file.is_open())
+        {
+                getline(file, lines[0]);
+                getline(file, lines[1]);
+                file.close();
+        }
+        else
+                cout << endl << "Error: Unable to open file" << endl;
+                
+        return lines;
 }
 
 /*
  * Method to compute the deliveryTime
  */
 double deliveryTime() {
-	double deliveryETA = 0.0;
-	return deliveryETA;
+        double deliveryETA = 0.0;
+        return deliveryETA;
 }
+
+int main() {
+        // test1
+        //WoodItem x;
+        //cout << x.price;
+
+        string* order = readInputFile("order.txt");
+        
+        // test2
+        cout << order[0] << endl << order[1] << endl;
+        return 0;
+}
+
